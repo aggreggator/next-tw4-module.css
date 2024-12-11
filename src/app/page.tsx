f1300bb1449b92e3d92d6ed0bb3241392c0c8419
Gregor Kaczmarczyk <github@aggreggator.de>
2024-12-11 03:48:25 +0100

    chore: adjust heading margin for improved layout on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col bg-gradient-to-t from-[hsl(40,58%,8%)] via-[hsl(40,58%,14%)] to-[hsl(40,58%,28%)] text-[hsl(40,58%,80%)] md:items-center md:justify-center">
       <div className="flex flex-col gap-8 px-4 pt-12 md:items-center md:justify-center">
-        <h1 className="border-b border-[hsl(40,58%,40%)] pb-3 pt-8 text-3xl tracking-wide text-[hsl(40,58%,80%)] md:-mt-24">
+        <h1 className="border-b border-[hsl(40,58%,40%)] pt-8 pb-3 text-3xl tracking-wide text-[hsl(40,58%,80%)] md:-mt-24">
           simple <span className="text-[hsl(40,58%,40%)]">handpan </span> app
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid sm:grid-cols-2 md:gap-8">
